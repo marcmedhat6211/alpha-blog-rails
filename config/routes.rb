6e6479f36f27f50bfd597285073e67004f0c8847
Marc Medhat <marcmedhat6211@gmail.com>
2021-08-29 20:22:17 +0200

    creating articles table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  #this is how you specify the root route
+  #controller #action
   root 'pages#home'
+  #request type, route name, controller, action
   get 'about', to: 'pages#about'
 end
+
