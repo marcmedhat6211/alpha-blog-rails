82d039eb6ed12b167e84b2e87bea51de2ede5081
Marc Medhat <marcmedhat6211@gmail.com>
2021-09-04 23:55:04 +0200

    styling the form partial file and the flash messages

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#this code bey5ally en lama input yeb2a 3aleh validation error mayet3melsh el div el zyada bta3et el validation
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
