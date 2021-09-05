a0eb2927841115f117c002bf6dd77c806898b1a3
Marc Medhat <marcmedhat6211@gmail.com>
2021-09-05 22:30:22 +0200

    adding the bcrypt gem and adding the password_digest column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_05_184033) do
+ActiveRecord::Schema.define(version: 2021_09_05_192315) do
 
   create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_09_05_184033) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
