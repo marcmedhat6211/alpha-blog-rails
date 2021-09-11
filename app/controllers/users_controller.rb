9275334a186315e08923eb3e00e2e44afc457596
marcmedhat6211 <marcmedhat6211@gmail.com>
2021-09-11 19:18:43 +0200

    layout cleanup and handling the user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
   
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
+    @articles = @user.articles
   end
 
   def new
@@ -16,7 +21,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
-      redirect_to root_path
+      redirect_to @user
     else
       render 'edit'
     end
