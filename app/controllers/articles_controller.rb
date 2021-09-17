d7674a410e821c2dd5a63b9fc1b29d4aac15f986
marcmedhat6211 <marcmedhat6211@gmail.com>
2021-09-17 21:31:48 +0200

    adding the option to select multiple categories for one articel from the frontend

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -95,7 +95,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
 
   def require_same_user
